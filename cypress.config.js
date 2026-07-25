47aae9c491bb4e53a136ab10e3e79fbd093c70ce
Carolinarosillo43@gmail.com <163232523+CarolinaRosillo@users.noreply.github.com>
2026-07-25 18:28:45 -0500

    borrar capertas results y report

--- cypress.config.js
+++ cypress.config.js
@@ -1,15 +1,13 @@
 const { defineConfig } = require("cypress");
-const { configureAllureAdapterPlugins } = require('@mmisty/cypress-allure-adapter/plugins');
+const allureWriter = require('@shelex/cypress-allure-plugin/writer');
 
 module.exports = defineConfig({
-  allowCypressEnv: false,
 
+  retries:1,
   e2e: {
     setupNodeEvents(on, config) {
-      // implement node event listeners here
-      configureAllureAdapterPlugins(on,
-config);
-return config;
+      allureWriter(on, config);
+      return config;
     },
   },
 });
--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -2,9 +2,11 @@ import { LoginMethods } from "./pages/login/login.methods"
 
 describe('template spec', () => {
   it('passes', () => {
+    const usuario = 'random01';
+    const contrasena = 'random01'
     cy.visit('https://demoblaze.com/')
     cy.get('a[data-target="#logInModal"]').click()
-    LoginMethods.login('username', 'password')
-    cy.wait(5000)
+    LoginMethods.login(usuario, contrasena)
+    cy.wait(10000)
   })
 })
