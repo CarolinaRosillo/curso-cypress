515989e5a047dea86dad300e8dbd78d63092a584
Carolinarosillo43@gmail.com <163232523+CarolinaRosillo@users.noreply.github.com>
2026-07-31 23:08:20 -0500

    Creacion de la pagina Place Order

---
+++ cypress/e2e/pages/place-order/place-order.data.js
@@ -0,0 +1,12 @@
+export class PlaceOrderData {
+    static get testData(){
+        return{
+            name: 'Test name',
+            country: 'United states',
+            city: 'Boston',
+            CreditCardNumber: '378282246310005',
+            month: 12,
+            year: 2024
+        }
+    } 
+    }
